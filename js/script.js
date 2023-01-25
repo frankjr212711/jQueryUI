40b7b590ddbf701c1ef0d7d64a6ee4b64f9df7a7
frankjay <frankjr212711@gmail.com>
2023-01-25 22:33:08 +0000

    still final patches

--- js/script.js
+++ js/script.js
@@ -17,17 +17,21 @@ const aboutSection = document.querySelector('.about-section');
 document.addEventListener('click', function(e) {
    if(e.target.classList.contains('view-project-btn')) {
     togglePortfolioPopup();
+    portfolioItemDetails(e.target.parentElement);
    }
  
 })
 
-function togglePortfolioPopup() {
+function togglePortfolioPopup () {
     document.querySelector('.portfolio-popup').classList.toggle('open');
     document.body.classList.toggle('hide-scrolling');
+    document.querySelector('.main').classList.toggle('fade-out');
 }
-document.querySelector('.pp-close').addEventListener('click', togglePortfolioPopup)
+function portfolioItemDetails (portfolioItem) {
+    document.querySelector('.pp-thumbnail img').src = portfolioItem.querySelector('.portfolio-item-thumbnail img').src;
+    document.querySelector('.pp-close').addEventListener('click', togglePortfolioPopup)
 
-
+}
 
 
 
