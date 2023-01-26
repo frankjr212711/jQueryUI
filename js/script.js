f3e6bf6af303f5b51132965ced766ed87e521b37
frankjay <frankjr212711@gmail.com>
2023-01-26 01:39:11 +0000

    about to sleep

--- js/script.js
+++ js/script.js
@@ -1,4 +1,41 @@
 
+
+//  Toggle Navbar 
+
+const navToggler = document.querySelector('.nav-toggler');
+    navToggler.addEventListener('click', () => {
+        hideSection();
+        toggleNavbar();
+    })
+
+    function hideSection() {
+        document.querySelector('section.active').classList.toggle('fade-out');
+    }
+    function toggleNavbar() {
+        document.querySelector('.header').classList.toggle('active');
+    }
+// Active section
+document.addEventListener('click', (e) => {
+    if(e.target.classList.contains('link-item') && e.target.hash !== '') {
+        const hash = e.target.hash;
+        if(e.target.classList.contains('nav-item')) {
+            toggleNavbar();
+        } else {
+            hideSection();
+            document.body.classList.remove('hide-scrolling');
+        }
+        setTimeout(() => {
+            document.querySelector('section.active').classList.remove('active', 'fade-out');
+            document.querySelector('e.target.hash').classList.add('active');
+            window.scrollTo(0, 0);
+            document.body.classList.remove('hide-scrolling');
+        }, 500);
+    }
+});
+
+
+
+
 const tabContainer = document.querySelector('.about-tabs');
 const aboutSection = document.querySelector('.about-section');
 
@@ -17,40 +54,36 @@ const aboutSection = document.querySelector('.about-section');
 document.addEventListener('click', function(e) {
    if(e.target.classList.contains('view-project-btn')) {
     togglePortfolioPopup();
+    document.querySelector('.portfolio-popup').scrollTo(0, 0);
     portfolioItemDetails(e.target.parentElement);
    }
- 
 })
 
 function togglePortfolioPopup () {
     document.querySelector('.portfolio-popup').classList.toggle('open');
     document.body.classList.toggle('hide-scrolling');
     document.querySelector('.main').classList.toggle('fade-out');
-}
+};
+
+// Close button
+document.querySelector('.pp-close').addEventListener('click', togglePortfolioPopup);
+
+// Hide popup when clicking outside of it 
+document.addEventListener('click', (e) => {
+    if(e.target.classList.contains('pp-inner')) {
+        togglePortfolioPopup();
+    };
+});
+
 function portfolioItemDetails (portfolioItem) {
     document.querySelector('.pp-thumbnail img').src = portfolioItem.querySelector('.portfolio-item-thumbnail img').src;
-    document.querySelector('.pp-close').addEventListener('click', togglePortfolioPopup)
+  
+
+    document.querySelector('.pp-header h3').innerHTML = portfolioItem.querySelector('.portfolio-item-title').innerHTML;
+    document.querySelector('.pp-body').innerHTML = portfolioItem.querySelector('.portfolio-item-details').innerHTML;
 
 }
 
 
 
-
-
-
-// $(function() {
-//     $("#todoList ul").sortable({
-//         items: "li:not('.listTitle, .addItem')",
-//         connectWith: 'ul',
-//         dropOnEmpty: true,
-//         placeholder: 'emptyspace'
-//     });
-
-//     $('input').keydown(function(e) {
-//         if(e.keyCode == 13) {
-//             let item = $(this).val();
-//             $(this).parent().parent().append('<li>' + item + '</li>')
-//         }
-//     })
-// })
 
